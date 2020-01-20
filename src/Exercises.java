8ca8f89df31bbf3bc889ca44df58f50f61ce0233
Jiggly-Jiggs <37905247+Jiggly-Jiggs@users.noreply.github.com>
2020-01-20 10:27:58 -0500

    initial commit

--- src/Exercises.java
+++ src/Exercises.java
@@ -1,66 +1,82 @@
 public class Exercises {
 
+	public static void main(String[] args) {
+
+		ProblemSet5 ps = new ProblemSet5();
+		// System.out.println(ps.commonEnd());
+		// System.out.println(ps.endsMeet());
+		// System.out.println(ps.difference());
+		// System.out.println(ps.biggest());
+		// System.out.println(ps.middle());
+		// System.out.println(ps.increasing());
+		// System.out.println(ps.everywhere());
+		// System.out.println(ps.consecutive());
+		// System.out.println(ps.balance());
+		// System.out.println(ps.clumps());
+
+	}
+
 	public boolean commonEnd(int[] a, int[] b) {
 		if (a == null || a.length == 0 || b == null || b.length == 0) {
 			return false;
 		}
-		
+
 		// write your code here
-		
+
 		return false;	// default return value to ensure compilation
 	}
-	
+
 	public String[] endsMeet(String[] values, int n) {
 		// write your code here
-		
+
 		return null;	// default return value to ensure compilation
 	}
-	
+
 	public int difference(int[] numbers) {
 		// write your code here
-		
+
 		return -1;		// default return value to ensure compilation
 	}
-	
+
 	public double biggest(double[] numbers) {
 		// write your code here
-		
+
 		return -1;		// default return value to ensure compilation
 	}
-	
+
 	public String[] middle(String[] values) {
 		// write your code here
-		
+
 		return null;	// default return value to ensure compilation
 	}
 
 	public boolean increasing(int[] numbers) {
 		// write your code here
-		
+
 		return false;	// default return value to ensure compilation
 	}
-	
+
 	public boolean everywhere(int[] numbers, int x) {
 		// write your code here
-		
+
 		return false;	// default return value to ensure compilation
 	}
-	
+
 	public boolean consecutive(int[] numbers) {
 		// write your code here
-		
+
 		return false;	// default return value to ensure compilation
 	}
-	
+
 	public boolean balance(int[] numbers) {
 		// write your code here
-		
+
 		return false;	// default return value to ensure compilation
 	}
-	
+
 	public int clumps(String[] values) {
 		// write your code here
-		
+
 		return -1;		// default return value to ensure compilation
 	}
 }
